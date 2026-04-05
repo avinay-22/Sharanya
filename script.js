5398b3f6b509daba5a400a86277e72d6c7050318
avinay-22 <vinayvinay2294@gmail.com>
2026-04-05 22:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = "This page is just for you... You make my life beautiful, and I’m so lucky to have you ❤️";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typingText").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 40);
+    }
+}
+
+window.onload = typeEffect;
+
+function showMessage() {
+    document.getElementById("loveMessage").style.display = "block";
+}
